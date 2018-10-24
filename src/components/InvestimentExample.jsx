7bb47a5e29eb16e9756c5fbc35c6d7280cefdd76
Sergio Marcilla <sermara70@gmail.com>
2018-10-24 23:33:50 +0200

    Create project configuration

---
+++ src/components/InvestimentExample.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export class InvestimentExample extends Component {
+    render(){
+        return (
+            <div className="investiment-example">
+                <h1>Investiment Example App</h1>
+            </div>
+        );
+    }
+}
+
+export default InvestimentExample;
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import InvestimentExample from './components/InvestimentExample';
+
+ReactDOM.render(<InvestimentExample />, document.getElementById('app'));
